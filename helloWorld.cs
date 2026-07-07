26b3a8a8d178511c2d87e4182607986e19559ab1
Eren Akkoç <ern.akkc@gmail.com>
2026-07-07 18:40:30 +0300

    first commit

---
+++ helloWorld.cs
@@ -0,0 +1,16 @@
+Console.WriteLine("Hello World!");
+Console.WriteLine("This is a simple C# program.");
+
+
+Console.WriteLine('b');
+
+Console.WriteLine(123);
+
+/*
+Float Type    Precision
+----------------------------
+float         ~6-9 digits
+double        ~15-17 digits
+decimal        28-29 digits
+*/
+
