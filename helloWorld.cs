99ce3dfee41bc182ba45513bee5f73e424bb8c31
Eren Akkoç <ern.akkc@gmail.com>
2026-07-07 18:51:43 +0300

    variable definition

--- helloWorld.cs
+++ helloWorld.cs
@@ -1,10 +1,6 @@
 Console.WriteLine("Hello World!");
 Console.WriteLine("This is a simple C# program.");
-
-
 Console.WriteLine('b');
-
-Console.WriteLine(123);
 
 /*
 Float Type    Precision
@@ -13,4 +9,27 @@ float         ~6-9 digits
 double        ~15-17 digits
 decimal        28-29 digits
 */
+Console.WriteLine(123);
+Console.WriteLine(12.2F);
+Console.WriteLine(12312.2131312m);
+
+// BOOLEAN
+Console.WriteLine(true);
+Console.WriteLine(false);
+
+// Veriable Definition
+char userOption;
+int gameScore;
+decimal particlesPerMillion;
+bool processedCustomer;
+
+userOption = 'a';
+gameScore = 100;
+processedCustomer = true;
+
+Console.WriteLine(userOption);
+Console.WriteLine(gameScore);
+
+var message = "Hello World!";
+Console.WriteLine(message);
 
