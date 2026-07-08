6779388d2a9ad04ad250e0ecec9ded7dfc82fb65
Eren Akkoç <ern.akkc@gmail.com>
2026-07-08 17:08:07 +0300

    formatting print options

---
+++ Project-6/Program.cs
@@ -0,0 +1,14 @@
+﻿string myStr = "Merhaba Dünya!";
+Console.WriteLine(myStr);
+
+string newStr = string.Format("{0} {1}!", "Merhaba", "Dünya");
+Console.WriteLine(newStr);
+
+decimal measurement = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement:N} units");
+Console.WriteLine($"Measurement: {measurement:N4} units"); 
+Console.WriteLine($"Measurement: {measurement:P20} units"); 
+
+string input = "Pad this";
+Console.WriteLine(input.PadLeft(12));
+Console.WriteLine(input.PadRight(12, '-')); 
