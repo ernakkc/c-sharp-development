88e4d82ec98dfeb4f3bf676d1c0bc3029280cd16
Eren Akkoç <ern.akkc@gmail.com>
2026-07-08 16:54:41 +0300

    array

---
+++ Project-5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics.Metrics;
+
+decimal a = 5.91211231m;
+Console.WriteLine(a); // 5.91211231
+
+int b = (int) a;
+Console.WriteLine(b); // 5
+
+int c = Convert.ToInt32(a);
+Console.WriteLine(c); // 6
+
+
+string myString = "10";
+int myInt = int.Parse(myString);
+Console.WriteLine(myInt.GetType()); // System.Int32
+
+
+
+// ARRAY
+string[] myArray = ["b","a","d","c"];
+
+Array.Sort(myArray);
+foreach (var item in myArray)
+{
+    Console.WriteLine(item); // a b c d
+}
+
+Array.Clear(myArray, 1,2);
+foreach (var item in myArray)
+{
+    Console.WriteLine(item); // a [] [] d
+}
+
+Array.Resize(ref myArray, 6);
+var counter = 0;
+foreach (var item in myArray)
+{
+    Console.WriteLine($"Index: {counter}\tValue: {item}");
+    counter++;
+    /*
+    Index: 0        Value: a
+Index: 1        Value: 
+Index: 2        Value: 
+Index: 3        Value: d
+Index: 4        Value: 
+Index: 5        Value: 
+*/
+}
+
+
+
