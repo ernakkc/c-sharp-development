c16c438278691fa3752df5f9bc34d84e356370ce
Eren Akkoç <ern.akkc@gmail.com>
2026-07-15 14:54:34 +0300

    updated files

--- Project-8/obj/Debug/net10.0/Project-8.AssemblyInfo.cs
+++ Project-8/obj/Debug/net10.0/Project-8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project-8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e6a938509afbc1d93484c544e47c4d5af6c527c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05b205f445a805be26c873818f5f65509aa9053d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project-8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project-8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
